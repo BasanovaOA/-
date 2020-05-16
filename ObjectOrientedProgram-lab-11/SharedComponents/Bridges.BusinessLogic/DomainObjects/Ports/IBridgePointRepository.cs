8082ac194cc03306ea9a520b5484513724d9549e
BasanovaOA <50922999+BasanovaOA@users.noreply.github.com>
2020-05-17 00:02:12 +0300

    Add files via upload

---
+++ ObjectOrientedProgram-lab-11/SharedComponents/Bridges.BusinessLogic/DomainObjects/Ports/IBridgePointRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Bridges.DomainObjects.Ports
+{
+    public interface IReadOnlyBridgePointRepository
+    {
+        Task<BridgePoint> GetBridgePoint(long id);
+
+        Task<IEnumerable<BridgePoint>> GetAllBridgePoints();
+
+        Task<IEnumerable<BridgePoint>> QueryBridgePoints(ICriteria<BridgePoint> criteria);
+
+    }
+
+    public interface IBridgePointRepository
+    {
+        Task AddBridgePoint(BridgePoint bridgepoint);
+
+        Task RemoveBridgePoint(BridgePoint bridgepoint);
+
+        Task UpdateBridgePoint(BridgePoint bridgepoint);
+    }
+}
