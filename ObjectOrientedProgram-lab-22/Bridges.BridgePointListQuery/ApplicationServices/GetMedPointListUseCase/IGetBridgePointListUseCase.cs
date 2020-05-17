3eeb8724daf60cd19db2cff9ae34c9494b60ddf4
BasanovaOA <50922999+BasanovaOA@users.noreply.github.com>
2020-05-17 15:21:41 +0300

    Add files via upload

---
+++ ObjectOrientedProgram-lab-22/Bridges.BridgePointListQuery/ApplicationServices/GetMedPointListUseCase/IGetBridgePointListUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bridges.ApplicationServices.Interfaces;
+
+namespace Bridges.ApplicationServices.GetBridgePointListUseCase
+{
+    public interface IGetBridgePointListUseCase : IUseCase<GetBridgePointListUseCaseRequest, GetBridgePointListUseCaseResponse>
+    {
+    }
+}
